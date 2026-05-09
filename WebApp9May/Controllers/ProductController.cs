8cd36bf654d2fde555c6f84ec1827a5d728c36ea
chandrashekharcom <chandrashekhar.mmmec09@gmail.com>
2026-05-09 11:13:21 +0530

    Add project files.

---
+++ WebApp9May/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp9May.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var products = _db.Products.ToList();
+            return View(products);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
